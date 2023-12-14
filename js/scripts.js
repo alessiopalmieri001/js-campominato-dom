a2da44bb50e8c9915c0acf9150491d184d8784be
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2023-12-14 12:44:06 +0100

    numero casuali

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,29 @@ bPlay.addEventListener('click', function(){
         let difficoltà = parseInt(document.getElementById('difficoltà').value);
         console.log(difficoltà);
         grid.innerHTML = "";
+
+        const randombomb = [];
+
+        for (let i = 0; i < 16; i++) {
+            // Genero un numero casuale
+            let bomb = generateRandomNumber(1, difficoltà);
+            console.log(bomb);
+        
+            // Controllo se questo numero appena generato è già presente nell'array
+            let foundInArray = randombomb.includes(bomb);
+            while (foundInArray == true) {
+                bomb = generateRandomNumber(1, difficoltà);
+                console.log(bomb);
+                foundInArray = randombomb.includes(bomb);
+            }
+        
+            randombomb.push(bomb);
+            
+            console.log(randombomb);
+        }
+        
+        console.log(randombomb);
+
         
         //creo un ciclo definito per avere dei numeri da 1 a 100
         for(i = 1; i <= difficoltà; i++){
@@ -23,17 +46,32 @@ bPlay.addEventListener('click', function(){
         cella.classList.add('cella', 'cell-' + difficoltà);
         //cosi aggiungo dentro le celle i numeri che mi escono 
         cella.append(i);
-        
-        
-        //creo un altro evento click per colorare le celle quando si attivano 
+
         cella.addEventListener('click', function(){
             //con questo comando sto dicendo di prendere il tag della variabile cella e aggiungi la classe active come un interrutore
-            this.classList.toggle('active');
+            
+            if(i === randombomb[0]){
+                this.classList.add('bomb')
+            }
+            else{this.classList.add('active');
             console.log(this.innerHTML);
+            }
         });
         //qui appendo cioè stampo nel container 
         grid.append(cella);
 
-    }
+        }
+
+
     
 });
+
+
+
+//funzioni
+
+function generateRandomNumber(min, max) {
+    const randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    return randNum;
+}
